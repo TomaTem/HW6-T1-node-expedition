91b22c63b937d37b955c5b3aac46993e72457fc6
tomatem <tem7720@gmail.com>
2023-04-17 13:10:04 +0300

    exp2 done with prettier

--- expedition2/src/functions.js
+++ expedition2/src/functions.js
@@ -1,37 +1,44 @@
-const fs = require('fs');
+const fs = require("fs");
 //получаем массив из файла
-const getArrFromFile = (url) => fs.readFileSync(url, 'utf8').split('\n').map(str => str.split(',').map(str => str.trim()))
+const getArrFromFile = (url) =>
+  fs
+    .readFileSync(url, "utf8")
+    .split("\n")
+    .map((str) => str.split(",").map((str) => str.trim()));
 
 //получаем объект из массивва
 function getObject(arr) {
-  let mainObj= {};
-  for (let i=1; i<arr.length; i++) {
+  let mainObj = {};
+  for (let i = 1; i < arr.length; i++) {
     arr[i].reduce((target, key, j) => {
-    target[arr[0][j]] = key;
-    return mainObj[i] = target;
-  }, {});
+      target[arr[0][j]] = key;
+      return (mainObj[i] = target);
+    }, {});
   }
   return mainObj;
-};
+}
 
-const crew = getArrFromFile('src/crew.txt');
+const crew = getArrFromFile("src/crew.txt");
 const crewObj = getObject(crew);
-const equipment = getArrFromFile('src/equipment.txt');
+const equipment = getArrFromFile("src/equipment.txt");
 const equipmentObj = getObject(equipment);
-const rockets = getArrFromFile('src/rockets.txt');
+const rockets = getArrFromFile("src/rockets.txt");
 const rocketsObj = getObject(rockets);
 
 //позволяет выбрать самого опытного капитана
 function getRightCaptain() {
-    let experience = 0;
-    let result = {};
-    for (key in crewObj) {
-      if (crewObj[key]['Должность'] == 'Капитан' && Number(crewObj[key]['Стаж']) > experience) {
-        experience = Number(crewObj[key]['Стаж']);
-        result = crewObj[key];
+  let experience = 0;
+  let result = {};
+  for (key in crewObj) {
+    if (
+      crewObj[key]["Должность"] == "Капитан" &&
+      Number(crewObj[key]["Стаж"]) > experience
+    ) {
+      experience = Number(crewObj[key]["Стаж"]);
+      result = crewObj[key];
     }
   }
-    return result;
+  return result;
 }
 
 //позволяет выбрать самого опытного бортмеханика среди женщин
@@ -39,11 +46,15 @@ function getRightEngineer() {
   let experience = 0;
   let result = {};
   for (key in crewObj) {
-    if (crewObj[key]['Должность'] == 'Бортмеханик' && crewObj[key]['пол (м/ж)'] == 'ж' && Number(crewObj[key]['Стаж']) > experience) {
-      experience = Number(crewObj[key]['Стаж']);
+    if (
+      crewObj[key]["Должность"] == "Бортмеханик" &&
+      crewObj[key]["пол (м/ж)"] == "ж" &&
+      Number(crewObj[key]["Стаж"]) > experience
+    ) {
+      experience = Number(crewObj[key]["Стаж"]);
       result = crewObj[key];
+    }
   }
-}
   return result;
 }
 
@@ -51,32 +62,38 @@ function getRightEngineer() {
 function getRightDoc() {
   let result = [];
   for (key in crewObj) {
-    if (crewObj[key]['Должность'] == 'Врач' && Number(crewObj[key]['Стаж']) > 5) {
+    if (
+      crewObj[key]["Должность"] == "Врач" &&
+      Number(crewObj[key]["Стаж"]) > 5
+    ) {
       result.push(crewObj[key]);
+    }
   }
-}
   return result;
 }
 
 //Позволяет отобрать все луноходы
-function getAllRover(){
+function getAllRover() {
   let result = [];
   for (key in equipmentObj) {
-    if (equipmentObj[key]['Тип оборудования'] == 'луноход') {
+    if (equipmentObj[key]["Тип оборудования"] == "луноход") {
       result.push(equipmentObj[key]);
+    }
   }
-}
   return result;
 }
 
 //позволяет выбрать только те луноходы, которые смогут прослужить больше 7 лет
-function getRightRovers(){
+function getRightRovers() {
   let result = [];
   for (key in equipmentObj) {
-    if (equipmentObj[key]['Тип оборудования'] == 'луноход' && Number(equipmentObj[key]['Предполагаемый срок службы (в годах)']) > 7) {
+    if (
+      equipmentObj[key]["Тип оборудования"] == "луноход" &&
+      Number(equipmentObj[key]["Предполагаемый срок службы (в годах)"]) > 7
+    ) {
       result.push(equipmentObj[key]);
+    }
   }
-}
   return result;
 }
 
@@ -84,25 +101,24 @@ function getRightRovers(){
 function getInterstellarRockets() {
   let result = [];
   for (key in rocketsObj) {
-    if (rocketsObj[key]['тип ракеты'] == 'межзвездная') {
+    if (rocketsObj[key]["тип ракеты"] == "межзвездная") {
       result.push(rocketsObj[key]);
+    }
   }
-}
   return result;
 }
-
 
 //позволяет выбрать ракету с максимальной дальностью полёта
 function getRightRocket() {
   let range = 0;
-    let result;
-    for (key in rocketsObj) {
-      if (Number(rocketsObj[key]['дальность полета (тыс. км.)']) > range) {
-        range = Number(rocketsObj[key]['дальность полета (тыс. км.)']);
-        result = rocketsObj[key];
+  let result;
+  for (key in rocketsObj) {
+    if (Number(rocketsObj[key]["дальность полета (тыс. км.)"]) > range) {
+      range = Number(rocketsObj[key]["дальность полета (тыс. км.)"]);
+      result = rocketsObj[key];
     }
   }
-    return result;
+  return result;
 }
 
 module.exports = {
